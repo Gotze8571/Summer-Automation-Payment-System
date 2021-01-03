c4556c3309661ffbba704fc2e71aee1f7217efa6
SummerTradingAPIv1 <telladesina100@gmail.com>
2021-01-03 16:31:21 +0100

    Add project files.

---
+++ SAP BusinessLogic/BusinessLogic/LogicUserDB.cs
@@ -0,0 +1,35 @@
+﻿using SAP_Model.DAL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace SAP_BusinessLogic.BusinessLogic
+{
+    public class LogicUserDB
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public readonly RespositoryContext context;
+        public LogicUserDB()
+        {
+            context = new RespositoryContext();
+        }
+        // validate username and password...
+        public bool ValidLogin()
+        {
+            var appId = ConfigurationManager.AppSettings["AppID"];
+            var appKey = ConfigurationManager.AppSettings["AppKey"];
+            bool isValidUser = false;
+            int thisUserId = 0;
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+            return false;
+        }
+    }
+}
